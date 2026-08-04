4d10c57a1bcdb9922d8d734ee0f472367ff9864a
Nguyễn Đắc Hiếu <ndhhieuu130304@gmail.com>
2026-08-04 21:32:02 +0700

    feat: create users and  students migration

---
+++ database/migrations/2026_08_04_141647_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->cascadeOnDelete();
+            $table->string('student_code')->unique();
+            $table->string('class');
+            $table->string('major');
+            $table->string('course');
+            $table->string('phone')->nullable();//ko phai luc nao sinh vien cung nhap sdt
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
